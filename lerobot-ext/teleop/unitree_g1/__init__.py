36ac90b47c33a8a7735aaad4a94d3b8d529f9a2d
mrwlker <miguel.walker.social@gmail.com>
2026-03-20 14:23:59 -0300

    Limpeza nos arquivo mortos

--- lerobot-ext/teleop/unitree_g1/__init__.py
+++ lerobot-ext/teleop/unitree_g1/__init__.py
@@ -1,7 +1,5 @@
 # Expondo o teclado para o LeRobot enxergar de cara
 from .keyboard_g1_arm import KeyboardG1Arm, KeyboardG1ArmConfig
 from .televuer import VuerTeleop, VuerTeleopConfig
-from .keyboard_g1_arm_v2 import KeyboardG1ArmV2, KeyboardG1ArmV2Config
-
 # Já aproveitando para expor o joystick também (caso você vá usar depois)
 #from .joy_teleoperator import JoystickG1Arms2, JoystickG1Arms2Config
